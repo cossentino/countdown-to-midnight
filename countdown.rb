3c7e016f8a43e88d6e3396c4b72ee22644375a64
Ian Cossentino <icossentino@gmail.com>
2020-11-09 21:41:13 -0500

    commit tree 138c841b3b826e6a949ef1b71672c5778145d06c

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
 #write your code here
 
-def countdown
+def countdown(n)
+    countdown = n
+    while countdown > 0
+        puts "#{countdown} SECOND(S)!"
+        #if countdown != 1
+         #   puts "#{countdown} SECONDS!"
+        #elsif countdown == 1
+        #    puts "#{countdown} SECOND!"
+        #end
+        countdown -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(n)
+    countdown = n
+    while countdown > 0
+        puts "#{countdown} SECOND(S)!"
+        sleep(1)
+        countdown -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
